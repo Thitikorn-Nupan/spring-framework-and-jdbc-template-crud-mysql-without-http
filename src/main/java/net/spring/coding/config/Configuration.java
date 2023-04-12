3227fe96e2ee8ab9d71a64badfbeaca73a0055c8
Thitikorn Beer <97865082+ttknpde-v@users.noreply.github.com>
2023-04-12 13:05:02 +0700

    Update Configuration.java

--- src/main/java/net/spring/coding/config/Configuration.java
+++ src/main/java/net/spring/coding/config/Configuration.java
@@ -6,15 +6,22 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan(basePackages="net.spring.coding") /* tells spring container looking at this folder */
+@PropertySource("classpath:informations/information.properties") /* @PropertySource("classpath:") using to include properties file */
 public class Configuration {
     private DriverManagerDataSource dataSource;
+    @Value("${jdbc.driver}") private String driver;
+    @Value("${jdbc.url}") private String url;
+    @Value("${jdbc.username}") private String username;
+    @Value("${jdbc.password}") private String password;
+
+    /* using @Value() to retrieve value from properties file */
     @Bean
     public DriverManagerDataSource dataSource () {
         dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3308/***");
-        dataSource.setUsername("***");
-        dataSource.setPassword("***");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource ;
     }
 }
